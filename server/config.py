8c9a1e6431621c1061b636f6469de27dd9338eed
Your Full Name <your.email@example.com>
2025-10-28 11:39:46 +0200

    Changed logs directory

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import datetime
 SESSION_START_TIME = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-LOGS_DIRECTORY = f"logs_{SESSION_START_TIME}"
+LOGS_DIRECTORY = f"logs/{SESSION_START_TIME}"
 HOST = "0.0.0.0"
 PORT = 9001
 MAX_CONNECTIONS_BACKLOG = 100
