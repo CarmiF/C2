f091ad33c1b7935857fb9781226648a4709a1098
Your Full Name <your.email@example.com>
2025-10-28 14:21:18 +0200

    Deleted line

--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 import datetime
-SESSION_START_TIME = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
+SESSION_START_TIME = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 LOGS_DIRECTORY = f"logs/{SESSION_START_TIME}"
 MAIN_LOG_FILE = f"{LOGS_DIRECTORY}/general_server_log"
 HOST = "0.0.0.0"
